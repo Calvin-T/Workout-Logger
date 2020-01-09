d9525fb500524018d8785a973767ba9f42f1a3a4
Michael Loh <michael.99.loh@gmail.com>
2020-01-08 23:37:10 -0500

    DB models

--- workoutlogger/logger/models.py
+++ workoutlogger/logger/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    email = models.CharField(max_length=30, primary_key=True)
+    age = models.IntegerField()
+    height = models.IntegerField()
+    weight = models.IntegerField()
+
+class Exercise(models.Model):
+    name = models.CharField(max_length=30, primary_key=True)
+    set = models.ManyToManyField(User, through='User_Exercise')
+
+class User_Exercise(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    exercise = models.ForeignKey(Exercise, on_delete=models.CASCADE)
+
+    repitions = models.IntegerField(null = True)
+    weight = models.IntegerField(null = True)
+    time = models.IntegerField(null = True)
+    distance = models.IntegerField(null = True)
+
+    date = models.DateField()
