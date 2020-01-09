98509321f7f9c5b679f4173b96f02cc95c18b01b
Michael Loh <michael.99.loh@gmail.com>
2020-01-08 23:37:34 -0500

    reverting back due to mis-commit

--- workoutlogger/logger/views.py
+++ workoutlogger/logger/views.py
@@ -8,6 +8,6 @@ def dashboard(request):
     if request.user.is_authenticated:
         print("LOGGED IN AS" + request.user.email)
         #should fix this back to dashboard.html
-        return render(request, "logger/dashboard-template.html")
+        return render(request, "logger/dashboard.html")
     else:
         return render(request, "logger/landing-page.html")
