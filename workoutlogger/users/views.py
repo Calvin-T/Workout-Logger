2e15d3bb18986a189f09c2f64ad71379f9ae68d4
Michael Loh <michael.99.loh@gmail.com>
2020-01-08 22:17:23 -0500

    Login Page

--- workoutlogger/users/views.py
+++ workoutlogger/users/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth import authenticate, logout
 from django.contrib.auth import login as auth_login
+from django.contrib.auth.forms import AuthenticationForm
 from .forms import CustomUserCreationForm
+
 
 def register(request):
     if request.method == 'POST':
@@ -22,7 +24,26 @@ def register(request):
     return render(request, 'users/register.html', {'form': form})
 
 def login(request):
-    return render(request, 'users/login.html')
+
+    if request.method == 'POST':
+        form = AuthenticationForm(request, request.POST)
+        if form.is_valid():
+            email = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username = email,
+                                password = password)
+            if user is not None:
+                auth_login(request, user)
+                messages.info(request, f"You are now logged in as {email}")
+                return redirect('logger-dashboard')
+            else:
+                messages.error(request, "invalid username or password")
+        else:
+            messages.error(request, "invalid username or password")
+
+    form = AuthenticationForm()
+    return render(request, 'users/login.html', {'form': form})
+
 
 def logout_view(request):
     logout(request)
